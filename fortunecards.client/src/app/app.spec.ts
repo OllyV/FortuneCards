0208545b4e65608e8540aa47ef8f3ec384e1feaf
olha <voa77777@gmail.com>
2026-06-04 18:41:16 +0100

    fix: update app.spec.ts to use provideZonelessChangeDetection and RouterModule

--- fortunecards.client/src/app/app.spec.ts
+++ fortunecards.client/src/app/app.spec.ts
@@ -1,5 +1,8 @@
-import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClientTesting, HttpTestingController } from '@angular/common/http/testing';
+import { provideZonelessChangeDetection } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { App } from './app';
 
 describe('App', () => {
@@ -10,11 +13,13 @@ describe('App', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [App],
-      imports: [HttpClientTestingModule]
+      imports: [RouterModule.forRoot([])],
+      providers: [
+        provideZonelessChangeDetection(),
+        provideHttpClient(),
+        provideHttpClientTesting(),
+      ],
     }).compileComponents();
-  });
-
-  beforeEach(() => {
     fixture = TestBed.createComponent(App);
     component = fixture.componentInstance;
     httpMock = TestBed.inject(HttpTestingController);
@@ -26,20 +31,5 @@ describe('App', () => {
 
   it('should create the app', () => {
     expect(component).toBeTruthy();
-  });
-
-  it('should retrieve weather forecasts from the server', () => {
-    const mockForecasts = [
-      { date: '2021-10-01', temperatureC: 20, temperatureF: 68, summary: 'Mild' },
-      { date: '2021-10-02', temperatureC: 25, temperatureF: 77, summary: 'Warm' }
-    ];
-
-    component.ngOnInit();
-
-    const req = httpMock.expectOne('/weatherforecast');
-    expect(req.request.method).toEqual('GET');
-    req.flush(mockForecasts);
-
-    expect(component.forecasts).toEqual(mockForecasts);
   });
 });
