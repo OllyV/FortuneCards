08d9b1a5f13735c85cf76029d36cc38448cd2489
olha <voa77777@gmail.com>
2026-06-09 12:17:28 +0100

    5: Design fixes, add gradients, open api for my friend.

---
+++ fortunecards.client/src/app/components/navigation-bar/navigation-bar.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'navigation-bar',
+  standalone: true,
+  templateUrl: './navigation-bar.html',
+  styleUrl: './navigation-bar.css',
+})
+export class NavigationBar {}
